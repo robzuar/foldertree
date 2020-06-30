d23db02bcc7a2a448c2b82a610520decbc7feae7
Roberto Zuñiga Araya <robzuar@Robertos-MacBook-Pro.local>
2020-06-29 23:30:19 -0400

    test console

--- src/AppBundle/Command/CorreoTestCommand.php
+++ src/AppBundle/Command/CorreoTestCommand.php
@@ -61,23 +61,22 @@ class CorreoTestCommand extends ContainerAwareCommand
                             </body>
                         </html>";
 
-        $strSubject = 'AVISO VENCIMIENTO ANTEPROYECTO';
+        $strSubject = 'Test';
 
 
         $strTo = 'roberto.zuniga.araya@gmail.com';
         $emailfrom = $this->getContainer()->getParameter('address');//'develsoftcl@gmail.com';//$input->getArgument('emailfrom');
-        $emailto = 'roberto.zuniga.araya@gmail.com';//'klowncero@gmail.com';// $anteproyecto->getCreatedBy()
 
 
-        if ($emailfrom && $emailto) {
+        $message = \Swift_Message::newInstance()
+            ->setSubject($strSubject)
+            ->setFrom($emailfrom)
+            ->setTo($strTo)
+            ->setContentType("text/html")
+            ->setBody($strBody);
+        $mail = $this->getContainer()->get('mailer')->send($message);
 
-            $message = \Swift_Message::newInstance()
-                ->setSubject($strSubject)
-                ->setFrom($emailfrom)
-                ->setTo($strTo)
-                ->setContentType("text/html")
-                ->setBody($strBody);
-            $this->getContainer()->get('mailer')->send($message);
-        }
+        $output->writeln($mail);
+
     }
 }
